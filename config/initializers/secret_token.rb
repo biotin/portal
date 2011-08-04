202bff8550ecaf1d0636f57b1a28b16e809ca0a4
biotin <qayibxanov@gmail.com>
2011-08-04 14:48:43 +0500

    readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portal::Application.config.secret_token = '7e24e2da1db9509c16d8e17d602fdb15d6de7cf13b3e3d09a8f39e00468357de37172d439ecae43ba2c96e966085650671253d8a0a7e37d15761f4c535d24ff3'

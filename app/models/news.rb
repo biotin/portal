33f5c0838988910560dc0e8af8d3272fc05462d3
biotin <qayibxanov@gmail.com>
2011-08-07 01:15:46 +0500

    Search2

--- app/models/news.rb
+++ app/models/news.rb
@@ -2,7 +2,7 @@ class News < ActiveRecord::Base
  validates :title, :description, presence: true
  def self.search(search)
   if search
-   where('description LIKE?', "%#{search}%")
+   where('description ||title LIKE?', "%#{search}%")
   else
    scoped
   end
